cf0f8f085704d48d264b938ebc1d0deca2597aa2
mhj212 <mhj212@gmail.com>
2016-11-08 15:06:46 -0500

    Show and tell

--- script.js
+++ script.js
@@ -23,4 +23,51 @@ $(document).ready(function(){
 		});
 	}
 
+
+	$.getJSON('menu3.json', function(data){
+		console.log(data);
+		$.each(data, function (i, data2){
+			if (data2.parentID == -1){
+				$('nav').append("<a class='"+data2.name+"class' href='#'>"+data2.name+"</a>");
+				
+				$('nav').after("<div class='"+data2.name+"dropdown' style='display:none;'></div");
+				
+			}
+			if (data2.parentID == 1){
+				$('.Homedropdown').append("<a href='#'>"+data2.name+"</a>");
+			}
+			if (data2.parentID == 5){
+				$(".Reportsdropdown").append("<a href='#'>"+data2.name+"</a>");
+			}
+			if (data2.parentID == 9){
+				$(".Statusdropdown").append("<a href='#'>"+data2.name+"</a>");
+			}
+
+		});
+	}).done(function(){
+		$('.Homeclass').click(function(){
+			$('.Homedropdown').toggle();
+			$('.Reportsdropdown').hide();
+			$('.Statusdropdown').hide();
+		});
+		$('.Reportsclass').click(function(){
+			$('.Reportsdropdown').toggle();
+			$('.Homedropdown').hide();
+			$('.Statusdropdown').hide();
+		});
+		$('.Statusclass').click(function(){
+			$('.Statusdropdown').toggle();
+			$('.Homedropdown').hide();
+			$('.Reportsdropdown').hide();
+		});
+
+		$(".Helpclass, .Logoutclass").click(function(){
+			$('.Statusdropdown').hide();
+			$('.Homedropdown').hide();
+			$('.Reportsdropdown').hide();
+		});
+	});
+
+
+
 });
